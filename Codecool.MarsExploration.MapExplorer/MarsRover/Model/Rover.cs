179c94dc33e1366e2673fd93b759339a5c7d444c
daniekk12 <alexandrudanielaka47@gmail.com>
2023-04-11 13:32:14 +0300

    f

--- Codecool.MarsExploration.MapExplorer/MarsRover/Model/Rover.cs
+++ Codecool.MarsExploration.MapExplorer/MarsRover/Model/Rover.cs
@@ -2,4 +2,5 @@
 
 namespace Codecool.MarsExploration.MapExplorer.MarsRover;
 
-public record Rover(string id, Coordinate position, int sight, IEnumerable<Coordinate> succesfullLocations);
+public record Rover(int id, Coordinate position, int sight, IEnumerable<Coordinate> succesfullLocations);
+

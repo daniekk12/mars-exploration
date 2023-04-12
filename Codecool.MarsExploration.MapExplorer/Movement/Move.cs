cbb2c9c9157bb9e272bd7adafb7d8d05a2adf1f2
daniekk12 <alexandrudanielaka47@gmail.com>
2023-04-12 10:19:08 +0300

    Scanner.cs + some Movement.cs changes

---
+++ Codecool.MarsExploration.MapExplorer/Movement/Move.cs
@@ -0,0 +1,28 @@
+﻿using Codecool.MarsExploration.MapExplorer.MarsRover;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Movement;
+
+public class MovementClass
+{
+    private Map map;
+    public void Move(Rover rover, Coordinate target)
+    {
+        int xDiff = target.X - rover.position.X;
+        int yDiff = target.Y - rover.position.Y;
+        
+        if (xDiff != 0)
+        {
+            int xStep = Math.Sign(xDiff);
+            rover.position = new Coordinate(rover.position.X + xStep, rover.position.Y);
+        }
+
+        if (yDiff != 0)
+        {
+            int yStep = Math.Sign(yDiff);
+            rover.position = new Coordinate(rover.position.X, rover.position.Y + yStep);
+        }
+    }
+
+}
---
+++ Codecool.MarsExploration.MapExplorer/Scanner/Scanner.cs
@@ -0,0 +1,33 @@
+﻿using Codecool.MarsExploration.MapExplorer.MarsRover;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+using System;
+
+namespace Codecool.MarsExploration.MapExplorer.Scanner;
+
+public class Scanner
+{
+    private Map _map;
+
+    public Scanner(Map map)
+    {
+        _map = map;
+    }
+    public Coordinate Scan(Rover rover,IEnumerable<string> resources)
+    {
+        for (var i = rover.position.X - rover.sight; i <= rover.position.X + rover.sight; i++)
+        {
+            for (var j = rover.position.Y - rover.sight; j <= rover.position.Y + rover.sight; j++)
+            {
+                if (i >= 0 && i < _map.Representation.GetLength(0) &&
+                    j >= 0 && j < _map.Representation.GetLength(1) &&
+                    resources.Contains(_map.Representation[i, j]))
+                {
+                Console.WriteLine($"x={rover.position.X} y={rover.position.Y}");
+                    return new Coordinate(i, j);
+                }
+            }
+        }
+        return null;
+    }
+}
